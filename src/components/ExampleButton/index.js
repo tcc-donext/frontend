8797f5c09e8db59aa8e9c17c51eed7ec767e42b9
Arthur Guerra <conta.acg2002@gmail.com>
2020-10-17 16:29:35 -0300

    ajustes iniciais

--- src/components/ExampleButton/index.js
+++ src/components/ExampleButton/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Button } from './styles';
 
 export default function ExampleButton(props) {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ExampleButton from 'components/ExampleButton';
 
 import { Container } from 'styles/pages/index';
