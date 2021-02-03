b37cc085db8c9a005d14e241988e70fb995ab204
arthur_cg2 <conta.acg2002@gmail.com>
2021-02-03 20:52:57 -0300

    adiciona scroller de paginas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,43 @@
 import PageLayout from 'components/layouts/PageLayout';
 
-import { Header, HeaderAnchor, Main, Footer } from 'styles/pages/index';
+import PageScroller from 'react-page-scroller';
+import { Header, HeaderAnchor, Footer } from 'styles/pages/index';
 import Button from 'components/Button';
 
 const Home = () => {
   return (
-    <>
-      <Header>
-        <ul>
-          <li>
-            <img src="/images/logo.png" alt="Logo DoNext" />
-          </li>
-          <li>
-            <HeaderAnchor>O que é?</HeaderAnchor>
-          </li>
-          <li>
-            <HeaderAnchor>Motivação</HeaderAnchor>
-          </li>
-          <li>
-            <HeaderAnchor>Quem Somos</HeaderAnchor>
-          </li>
-          <li>
-            <HeaderAnchor>Contato</HeaderAnchor>
-          </li>
-          <li>
-            <Button>Logar</Button>
-          </li>
-        </ul>
-      </Header>
-      <Main></Main>
-      <Footer></Footer>
-    </>
+    <PageScroller>
+      <section className="welcome">
+        <Header>
+          <ul>
+            <li>
+              <img src="/images/logo.png" alt="Logo DoNext" />
+            </li>
+            <li>
+              <HeaderAnchor>O que é?</HeaderAnchor>
+            </li>
+            <li>
+              <HeaderAnchor>Motivação</HeaderAnchor>
+            </li>
+            <li>
+              <HeaderAnchor>Quem Somos</HeaderAnchor>
+            </li>
+            <li>
+              <HeaderAnchor>Contato</HeaderAnchor>
+            </li>
+            <li>
+              <Button>Logar</Button>
+            </li>
+          </ul>
+        </Header>
+      </section>
+      <section className="what">O que é</section>
+      <section className="motivation">Motivação</section>
+      <section className="who">
+        Quem somos
+        <Footer>Contato</Footer>
+      </section>
+    </PageScroller>
   );
 };
 
--- src/styles/pages/index.js
+++ src/styles/pages/index.js
@@ -2,5 +2,4 @@ import styled from 'styled-components';
 
 export const Header = styled.header``;
 export const HeaderAnchor = styled.a``;
-export const Main = styled.main``;
 export const Footer = styled.footer``;
