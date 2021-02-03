99972c578715834149ee505e9c522157cd24295e
arthur_cg2 <conta.acg2002@gmail.com>
2021-02-03 19:36:57 -0300

    setup inicial da pagina inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,39 @@
-export default function Home() {
+import PageLayout from 'components/layouts/PageLayout';
+
+import { Header, HeaderAnchor, Main, Footer } from 'styles/pages/index';
+import Button from 'components/Button';
+
+const Home = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Header>
+        <ul>
+          <li>
+            <img src="/images/logo.png" alt="Logo DoNext" />
+          </li>
+          <li>
+            <HeaderAnchor>O que é?</HeaderAnchor>
+          </li>
+          <li>
+            <HeaderAnchor>Motivação</HeaderAnchor>
+          </li>
+          <li>
+            <HeaderAnchor>Quem Somos</HeaderAnchor>
+          </li>
+          <li>
+            <HeaderAnchor>Contato</HeaderAnchor>
+          </li>
+          <li>
+            <Button>Logar</Button>
+          </li>
+        </ul>
+      </Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
   );
-}
+};
+
+Home.layout = PageLayout;
+
+export default Home;
---
+++ src/styles/pages/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Header = styled.header``;
+export const HeaderAnchor = styled.a``;
+export const Main = styled.main``;
+export const Footer = styled.footer``;
