930b0d9a16637df2aa34fe1e7245a506224ce648
arthur_cg2 <conta.acg2002@gmail.com>
2021-02-04 22:57:20 -0300

    simplifica estrutura do botao e adiciona prop inverted

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,34 @@
-import StyledButton from './styles';
+import styled, { css } from 'styled-components';
 
-const Button = props => {
-  return <StyledButton {...props} />;
-};
-
-export default Button;
+export default styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: none;
+  border: none;
+  border-radius: 3pt;
+  ${({ inverted }) =>
+    inverted &&
+    css`
+      border: 2pt solid #e37fd6;
+    `};
+  background-color: ${({ inverted }) => {
+    return inverted ? '#fff' : '#e37fd6';
+  }};
+  color: ${({ inverted }) => {
+    return inverted ? '#e37fd6' : '#fff';
+  }};
+  font-size: ${props => props.fontSize || '1.2em'};
+  font-weight: 300;
+  width: ${props => props.width || '6vw'};
+  height: ${props => props.height || '4.75vh'};
+  &:hover {
+    background-color: ${({ inverted }) => {
+      return inverted ? '#e37fd6' : '#dc61cc';
+    }};
+    color: #fff;
+  }
+  &:active {
+    background-color: #d952c7;
+  }
+`;
