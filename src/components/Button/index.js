5b1857058c25a00cae9d561899d9ef09398fde8e
arthur_cg2 <conta.acg2002@gmail.com>
2021-02-04 22:49:48 -0300

    simplifica estrutura do botao

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,16 +1,42 @@
-import StyledButton from './styles';
+import styled, { css } from 'styled-components';
 
-/**
- * Props aceitos para o Button:
- * inverted: boolean
- * fontSize: string
- * height: string
- * width: string
- * obs.: as strings tem que ter medida, como px, pt, etc.
- */
+export default styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-const Button = props => {
-  return <StyledButton {...props} />;
-};
+  outline: none;
+  border: none;
+  border-radius: 3pt;
 
-export default Button;
+  ${({ inverted }) =>
+    inverted &&
+    css`
+      border: 2pt solid #e37fd6;
+    `};
+
+  background-color: ${({ inverted }) => {
+    return inverted ? '#fff' : '#e37fd6';
+  }};
+
+  color: ${({ inverted }) => {
+    return inverted ? '#e37fd6' : '#fff';
+  }};
+
+  font-size: ${props => props.fontSize || '1.2em'};
+  font-weight: 300;
+
+  width: ${props => props.width || '6vw'};
+  height: ${props => props.height || '4.75vh'};
+
+  &:hover {
+    background-color: ${({ inverted }) => {
+      return inverted ? '#e37fd6' : '#dc61cc';
+    }};
+    color: #fff;
+  }
+
+  &:active {
+    background-color: #d952c7;
+  }
+`;
