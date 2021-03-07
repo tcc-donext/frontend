1c566fb4d278ddcd8befc106bb9cb881bb9c8ea0
Wagner Alexsander <wagnera.cviana@gmail.com>
2021-03-07 20:50:02 -0300

    Capitura das informações das ongs para colocar no Perfil
    Capitura das informações das ongs para colocar no Perfil

--- src/pages/perfilOng.js
+++ src/pages/perfilOng.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { useAuth } from './../contexts/auth';
+import api from 'services/api';
+import { useAuth, ong } from './../contexts/auth';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -10,24 +11,47 @@ import Input from 'components/Input';
 
 const PerfilOng = () => {
   const { signed, user } = useAuth();
+  const [ongData, setOngData] = useState(null);
   const router = useRouter();
+
+  const infOng = async id => {
+    try {
+      const response = await api.get(`/ongs/${id}`);
+
+      setOngData(response.data);
+    } catch (err) {
+      console.warn(`Não foi possível recuperar as informações da Ong. ${err}`);
+      return null;
+    }
+  };
 
   useEffect(() => {
     if (!signed) {
       router.push('/');
     }
-  }, []);
+    infOng(user.id);
+  }, [ongData]);
+
+  const [
+    nom_ong = 'ongData[0].nom_ONG',
+    telefone = 'ongData[1].nro_telefone',
+    email = 'ongData[1].des_email',
+    cep = 'ongData[0].nro_cep',
+    uf = '',
+    endereco = 'ongData[0].des_endereco',
+    foto = 'user.image'
+  ] = [];
 
   return (
     <Container>
       <form>
-        <Input label="Nome da Ong" type="text" width="13vw" />
-        <Input label="Telefone" type="text" width="13vw" />
-        <Input label="Email" type="Email" width="30vw" />
-        <Input label="CEP" type="numeric" width="13vw" />
-        <Input label="UF" type="text" width="13vw" />
-        <Input label="Endereço" type="text" width="30vw" />
-        <Input label="Senha" type="password" width="15vw" />
+        <Input label="Nome da Ong" type="text" width="13vw" value={nom_ong} />
+        <Input label="Telefone" type="text" width="13vw" value={telefone} />
+        <Input label="Email" type="Email" width="30vw" value={email} />
+        <Input label="CEP" type="numeric" width="13vw" value={cep} />
+        <Input label="UF" type="text" width="13vw" value={uf} />
+        <Input label="Endereço" type="text" width="30vw" value={endereco} />
+        <Input label="Senha" type="password" width="15vw" value="*****" />
         <div className="buttonsContainer">
           <Button width="100%" height="8vh" fontSize="1.8em">
             Salvar
