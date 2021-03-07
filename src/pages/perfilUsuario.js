f132ab15be2448a05dc3928961ad97d8aab1b9c0
GzinSocker <gabriel.cama@outlook.com>
2021-03-06 21:48:11 -0300

    Integração do perfil do doador com o backend (Sem autenticação e estilização da mensagem de resultado)

--- src/pages/perfilUsuario.js
+++ src/pages/perfilUsuario.js
@@ -7,35 +7,80 @@ import { Container, Image, Divider } from 'styles/pages/perfilUsuario.js';
 import Button from 'components/Button';
 import Input from 'components/Input';
 
-
-
 const PerfilUsuario = () => {
-
+  const [id, setId] = useState(1);
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [telefone, setTelefone] = useState('');
   const [ddd, setDDD] = useState('');
-  
-  useEffect(() => {
+  const [doacoesCampanha, setDoacoesCampanha] = useState();
+  const [doacoesInstituicao, setDoacoesInstituicao] = useState();
 
-    api.get('/doador/1').then((response) => {
+  useEffect(async () => {
+    await api.get(`/doador/${id}`).then(response => {
       const data = response.data[0];
       setNome(data['nom_doador']);
       setEmail(data['des_email']);
-      setTelefone(data['nro_telefone'])
+      setTelefone(data['nro_telefone']);
       setDDD(data['nro_ddd']);
       console.log(data);
-    })
+    });
+    await api.get(`/contribuicao/${id}`).then(response => {
+      const data = response.data[0];
+      setDoacoesCampanha(data['doacoesCampanhas']);
+      setDoacoesInstituicao(data['doacoesInstituicoes']);
+      console.log(data);
+    });
   }, []);
+
+  const saveUdpatedName = async e => {
+    e.preventDefault();
+    let data = {};
+    await api.get(`/doador/${id}`).then(response => {
+      data = response.data[0];
+    });
+    data['nom_doador'] = nome;
+    const result = await api.put(`/doador/${id}`, data);
+    console.log(result);
+    // Resultado do upadate
+    // Botar uma mensagem estilizada (Status: 200 -> Deu certo / Status: 400 -> Deu pau)
+    if (result.status == '200') alert('Alterado!');
+    else if (result.status == '400') alert(result.data.error);
+  };
 
   return (
     <Container>
       <form>
-        <Input label="Nome" type="text"  width="29vw"  onChange={(e) => setNome(e.target.value)} value={nome}  />
-        <Input label="Telefone" type="text"  width="14vw" disabled readOnly value={`(${ddd}) ${telefone}`} />
-        <Input label="Email" type="Email"  width="29vw" disabled readOnly value={email} />
+        <Input
+          label="Nome"
+          type="text"
+          width="29vw"
+          onChange={e => setNome(e.target.value)}
+          value={nome}
+        />
+        <Input
+          label="Telefone"
+          type="text"
+          width="14vw"
+          disabled
+          readOnly
+          value={`(${ddd}) ${telefone}`}
+        />
+        <Input
+          label="Email"
+          type="Email"
+          width="29vw"
+          disabled
+          readOnly
+          value={email}
+        />
         <div className="buttonsContainer">
-          <Button width="100%" height="8vh" fontSize="1.8em">
+          <Button
+            width="100%"
+            height="8vh"
+            fontSize="1.8em"
+            onClick={saveUdpatedName}
+          >
             Salvar
           </Button>
         </div>
@@ -46,22 +91,29 @@ const PerfilUsuario = () => {
           alt="Desenho decorativo para a página"
         />
         <h1>Nome da Pessoa Completo Silva</h1>
-        <h2>R$ 750,00</h2>
+        <h2>{doacoesCampanha}</h2>
         <h3>Doado para campanhas</h3>
-        <h2>R$ 300,00</h2>
+        <h2>{doacoesInstituicao}</h2>
         <h3>Doado para instituições</h3>
       </aside>
       <span className="Desconectar">
-      <Button className="botaoDesconectar" onClick={async event => {
-              event.preventDefault();
-              const success = await logout();
-              if (success) router.push('/home');
-            }}><Image className="logoutIco" src="/images/logout.png"
-          alt="Icone Logout"></Image>Desconectar</Button>
+        <Button
+          className="botaoDesconectar"
+          onClick={async event => {
+            event.preventDefault();
+            const success = await logout();
+            if (success) router.push('/home');
+          }}
+        >
+          <Image
+            className="logoutIco"
+            src="/images/logout.png"
+            alt="Icone Logout"
+          ></Image>
+          Desconectar
+        </Button>
       </span>
-      
     </Container>
-    
   );
 };
 
