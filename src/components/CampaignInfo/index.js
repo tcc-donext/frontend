589251fa2f38b02f58083e2aac1d8b2dfe691a1d
Wagner Alexsander <wagnera.cviana@gmail.com>
2021-03-22 23:12:04 -0300

    Página da realod depois de criar ou alterar campanha
    Página da realod depois de criar ou alterar campanha

--- src/components/CampaignInfo/index.js
+++ src/components/CampaignInfo/index.js
@@ -14,14 +14,12 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useState, useEffect } from 'react';
 import { Formik, Form } from 'formik';
-import MaskedInput from 'react-text-mask'
-import createNumberMask from 'text-mask-addons/dist/createNumberMask'
-import moment from 'moment'
-import { useAuth } from 'contexts/auth'
-import api from 'services/api'
-
-
-
+import MaskedInput from 'react-text-mask';
+import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+import moment from 'moment';
+import { useAuth } from 'contexts/auth';
+import api from 'services/api';
+import { useRouter } from 'next/router';
 
 const defaultMaskOptions = {
   prefix: 'R$',
@@ -30,11 +28,11 @@ const defaultMaskOptions = {
   thousandsSeparatorSymbol: '.',
   allowDecimal: true,
   decimalSymbol: ',',
-  decimalLimit: 2, 
-  integerLimit: 10, 
+  decimalLimit: 2,
+  integerLimit: 10,
   allowNegative: false,
-  allowLeadingZeroes: false,
-}
+  allowLeadingZeroes: false
+};
 const curruncyStyle = {
   backgroundColor: '#ebebeb',
   color: '#403e4d',
@@ -42,28 +40,31 @@ const curruncyStyle = {
   border: 'none',
   fontSize: '1.25em',
   width: '32vw'
-}
+};
 
 const CampaignInfo = props => {
-  const [startDate, setStartDate] = useState(props.campaign ? new Date(props.campaign.dat_inicio) : null);
-  const [endDate, setEndDate] = useState(props.campaign ? new Date(props.campaign.dat_fim)  : null);
+  const [startDate, setStartDate] = useState(
+    props.campaign ? new Date(props.campaign.dat_inicio) : null
+  );
+  const [endDate, setEndDate] = useState(
+    props.campaign ? new Date(props.campaign.dat_fim) : null
+  );
   const { user } = useAuth();
-  
+  const router = useRouter();
 
   const [fileInputState, setFileInputState] = useState('');
   const [previewSource, setPreviewSource] = useState('/images/ebebeb.jpg');
   const [selectedFile, setSelectedFile] = useState();
 
-  const [categorias,setCategorias] = useState([])
+  const [categorias, setCategorias] = useState([]);
 
   useEffect(() => {
-    async function getCategorias(){
-    let response = await api.get('/categorias')
-    setCategorias(response.data)
+    async function getCategorias() {
+      let response = await api.get('/categorias');
+      setCategorias(response.data);
     }
-    getCategorias()
+    getCategorias();
   }, []);
-
 
   const handleFileInputChange = e => {
     const file = e.target.files[0];
@@ -83,47 +84,50 @@ const CampaignInfo = props => {
   const CurrencyInput = ({ maskOptions, ...inputProps }) => {
     const currencyMask = createNumberMask({
       ...defaultMaskOptions,
-      ...maskOptions,
-    })
-  
-    return <MaskedInput mask={currencyMask} {...inputProps} />
-  }
+      ...maskOptions
+    });
 
-  const SubmitData = async (values,type) =>{
-    if(type==1){
-      try{
-        const response = await api.post("campanhas", values)
-        console.log(response)
+    return <MaskedInput mask={currencyMask} {...inputProps} />;
+  };
 
-      }catch(err){
-        console.log(err)
+  const SubmitData = async (values, type) => {
+    if (type == 1) {
+      try {
+        const response = await api.post('campanhas', values);
+        console.log(response);
+        router.reload();
+      } catch (err) {
+        console.log(err);
       }
-  }else{
-    try{
-      const response = await api.put(`campanhas/${props.campaign.seq_campanha}`, values)
-      console.log(response)
-
-    }catch(err){
-      console.log(err)
+    } else {
+      try {
+        const response = await api.put(
+          `campanhas/${props.campaign.seq_campanha}`,
+          values
+        );
+        console.log(response);
+        router.reload();
+      } catch (err) {
+        console.log(err);
+      }
     }
-  }
-  }
-
-
+  };
 
   return (
-    
     <Formik
       initialValues={{
         nome: props.campaign ? props.campaign.des_titulo : null,
         Descricao: props.campaign ? props.campaign.des_geral : null,
-        categoria:  props.campaign ? props.campaign.cod_categoria : null, 
-        objetivo: props.campaign ? props.campaign.vlr_objetivo  : null,
-        dataInicio:  props.campaign ? moment(props.campaign.dat_inicio).format('MM/DD/YYYY')  : null,
-        dataFim:  props.campaign ? moment(props.campaign.dat_fim).format('MM/DD/YYYY')  : null, 
+        categoria: props.campaign ? props.campaign.cod_categoria : null,
+        objetivo: props.campaign ? props.campaign.vlr_objetivo : null,
+        dataInicio: props.campaign
+          ? moment(props.campaign.dat_inicio).format('MM/DD/YYYY')
+          : null,
+        dataFim: props.campaign
+          ? moment(props.campaign.dat_fim).format('MM/DD/YYYY')
+          : null
       }}
-      onSubmit={ async (values) => {
-
+      onSubmit={async values => {
         const data = {
           id_ong: user.id,
           des_titulo: values.nome,
@@ -131,27 +135,25 @@ const CampaignInfo = props => {
           cod_categoria: values.categoria,
           dat_inicio: values.dataInicio,
           dat_fim: values.dataFim,
-          vlr_objetivo: values.objetivo,
+          vlr_objetivo: values.objetivo
+        };
+        if (!selectedFile) {
+          props.campaign ? SubmitData(data, 2) : SubmitData(data, 1);
         }
-        if (!selectedFile){
-          props.campaign ? SubmitData(data,2): SubmitData(data,1)
-        } 
         const reader = new FileReader();
 
         reader.readAsDataURL(selectedFile);
         reader.onloadend = () => {
-          data.img_campanha = reader.result
-          props.campaign ? SubmitData(data,2): SubmitData(data,1)
+          data.img_campanha = reader.result;
+          props.campaign ? SubmitData(data, 2) : SubmitData(data, 1);
         };
         reader.onerror = () => {
-            console.error('AHHHHHHHH!!');
-            setErrMsg('something went wrong!');
+          console.error('AHHHHHHHH!!');
+          setErrMsg('something went wrong!');
         };
-
       }}
       render={({ values, errors, touched, handleChange, handleSubmit }) => (
-        
-        <Form style={{ height: '99%' }} onSubmit={handleSubmit} >
+        <Form style={{ height: '99%' }} onSubmit={handleSubmit}>
           <Container>
             <FormLayout>
               <InputField>
@@ -176,25 +178,32 @@ const CampaignInfo = props => {
               </InputField>
               <InputField>
                 <label>Categoria</label>
-                <SelectArea 
-                  type="select" name="categoria"
+                <SelectArea
+                  type="select"
+                  name="categoria"
                   onChange={handleChange}
                   value={values.categoria}
                   id="categoria"
                 >
-                  { categorias.map((categoria,index) =>{
-              return <option key={index} value={categoria.cod_categoria}>{categoria.nom_categoria}</option>
+                  {categorias.map((categoria, index) => {
+                    return (
+                      <option key={index} value={categoria.cod_categoria}>
+                        {categoria.nom_categoria}
+                      </option>
+                    );
                   })}
                 </SelectArea>
               </InputField>
               <InputField>
                 <label>Objetivo</label>
-                <CurrencyInput 
-                placeholder="R$0.00" type="text" style={curruncyStyle}
-                name="objetivo"
-                onChange={handleChange}
-                value={values.objetivo}
-                id="objetivo"
+                <CurrencyInput
+                  placeholder="R$0.00"
+                  type="text"
+                  style={curruncyStyle}
+                  name="objetivo"
+                  onChange={handleChange}
+                  value={values.objetivo}
+                  id="objetivo"
                 />
                 {console.log(values)}
               </InputField>
@@ -207,8 +216,8 @@ const CampaignInfo = props => {
                     dateFormat="dd/MM/yyyy"
                     selected={startDate}
                     onChange={date => {
-                      var selectedDateStr = moment(date).format('MM/DD/YYYY')
-                      setStartDate(date)
+                      var selectedDateStr = moment(date).format('MM/DD/YYYY');
+                      setStartDate(date);
                       values.dataInicio = selectedDateStr;
                     }}
                     placeholderText="ínicio"
@@ -218,9 +227,9 @@ const CampaignInfo = props => {
                     dateFormat="dd/MM/yyyy"
                     selected={endDate}
                     onChange={date => {
-                      var selectedDateStr = moment(date).format('MM/DD/YYYY')
-                      console.log(date)
-                      setEndDate(date)
+                      var selectedDateStr = moment(date).format('MM/DD/YYYY');
+                      console.log(date);
+                      setEndDate(date);
                       values.dataFim = selectedDateStr;
                     }}
                     placeholderText="fim"
